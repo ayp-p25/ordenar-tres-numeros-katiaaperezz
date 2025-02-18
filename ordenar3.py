96f48634dfc73068b066ce0d694e4cdfb88ae612
katiaaperezz <katiap.perez@iteso.mx>
2025-02-18 13:04:22 -0600

    Ordenar 3 4to intento

--- ordenar3.py
+++ ordenar3.py
@@ -17,9 +17,9 @@ elif numero1 <= numero2 and numero2 <= numero3:
     resultado = str(numero3) + " " + str(numero2) + " " + str(numero1) 
 elif numero2 >= numero3 and numero3 >= numero1:
     resultado = str(numero2) + " " + str(numero3) + " " + str(numero1)          
-elif numero2 >= numero3 and numero3 <= numero1:
+elif numero2 >= numero3 and numero1 <= numero3:
     resultado = str(numero2) + " " + str(numero1) + " " + str(numero3)
-elif numero1 <= numero2 and numero1 >= numero3: 
-    resultado = str(numero2) + " " + str(numero1) + " " + str(numero3)
+elif numero1 >= numero2 and numero2 >= numero3:
+    resultado = str(numero1) + " " + str(numero2) + " " + str(numero3)
 # Salidas
 print(resultado)
