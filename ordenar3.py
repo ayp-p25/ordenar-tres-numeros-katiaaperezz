ed789c23f27947f552f69b1a55629aecb61bbf55
katiaaperezz <katiap.perez@iteso.mx>
2025-02-18 12:32:15 -0600

    Ordenar 3 1er intento

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,21 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Ordenar tres números
 """
 
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+numero1 = int(input("Primer número: "))
+numero2 = int(input("Segundo número: "))
+numero3 = int(input("Tercer número: "))
 
 # Proceso
-
-
+if numero1 >= numero2  and numero1 >= numero3: 
+    resultado = str(numero1) + " " + str(numero2) + " " + str(numero3)
+elif numero1 >= numero2 and numero1 <= numero3:
+    resultado = str(numero3) + " " + str(numero1) + " " + str(numero2)
+elif numero1 <= numero2 and numero2 <= numero3: 
+    resultado = str(numero3) + " " + str(numero2) + " " + str(numero1) 
+elif numero2 >= numero3 and numero3 >= numero1:
+    resultado = str(numero2) + " " + str(numero3) + " " + str(numero1)          
 # Salidas
-print(salida)
+print(resultado)
