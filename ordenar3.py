8a1a731957059b0153f487071a2c827438ffba51
katiaaperezz <katiap.perez@iteso.mx>
2025-02-19 19:15:58 -0600

    Ordenar 3 5to intento

--- ordenar3.py
+++ ordenar3.py
@@ -9,8 +9,8 @@ numero2 = int(input("Segundo número: "))
 numero3 = int(input("Tercer número: "))
 
 # Proceso
-if numero1 >= numero2  and numero3 >= numero2: 
-    resultado = str(numero1) + " " + str(numero3) + " " + str(numero2)
+if numero1 >= numero2  and numero3 >= numero1: 
+    resultado = str(numero3) + " " + str(numero1) + " " + str(numero2)
 elif numero1 >= numero2 and numero1 <= numero3:
     resultado = str(numero3) + " " + str(numero1) + " " + str(numero2)
 elif numero1 <= numero2 and numero2 <= numero3: 
@@ -21,5 +21,7 @@ elif numero2 >= numero3 and numero1 <= numero3:
     resultado = str(numero2) + " " + str(numero1) + " " + str(numero3)
 elif numero1 >= numero2 and numero2 >= numero3:
     resultado = str(numero1) + " " + str(numero2) + " " + str(numero3)
+elif numero3 >= numero2 and numero2 >= numero1:
+    resultado = str(numero3) + " " + str(numero2) + " " + str(numero1)
 # Salidas
 print(resultado)
